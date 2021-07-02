275e0bf7b2f230b83a1f372b535ff20b17e6c748
techboolo <teklufa@gmail.com>
2021-07-02 21:09:49 +0300

    use some static data

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.json({ message: "it is working" })
+const notes = [
+  {
+    id: 1,
+    content: "html is easy",
+    date: new Date(),
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser execute js",
+    date: new Date(),
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are methods of HTTP protocol",
+    date: new Date(),
+    important: true
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
 })
 
 module.exports = app;

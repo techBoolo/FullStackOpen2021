12eca5a2828a70477a622e8fe76e2f99fca8a3de
techboolo <teklufa@gmail.com>
2021-07-03 08:54:00 +0300

    ex 3.1 phonebook backend, step1

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express');
 
 const app = express();
+const people = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "045- 234342"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "23-45-299342"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-01-014942"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppen",
+    "number": "39-01-853589"
+  }
+]
 
-app.use((req, res, next) => {
-  res.json({ message: "it is working" })
+app.use('/api/people', (req, res, next) => {
+  res.json(people)
 })
 
 module.exports = app;

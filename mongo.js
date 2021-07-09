e8d475ae31f49be7493a1f3bac9553c1f0ae5e99
techboolo <teklufa@gmail.com>
2021-07-09 09:49:05 +0300

    part 3c, basic data saving and retrieving from the database

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/test';
+
+mongoose.connect(url, {
+  useUrlNewParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+
+const noteSchema = mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema);
+
+// Create new note
+//const note = new Note({
+//  content: "new note",
+//  date: new Date(),
+//  important: Math.random() ? 'true' : 'false'
+//})
+//note.save()
+//  .then(result => {
+//    console.log(result);
+//    mongoose.connection.close();
+//  })
+//
+//  Fetch all notes
+//Note.find({})
+//  .then(result => {
+//    result.forEach(note => {
+//      console.log(note) 
+//    }) 
+//    mongoose.connection.close();
+//  })
+//  Fetch important notes only
+Note.find({ important: true})
+  .then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close();
+  })
